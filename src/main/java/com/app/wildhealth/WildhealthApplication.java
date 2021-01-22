7a9d344db877b0fc5afc486d20074579ea05de7c
IW.MOON <=>
2021-01-22 12:43:21 +0900

    init

---
+++ src/main/java/com/app/wildhealth/WildhealthApplication.java
@@ -0,0 +1,13 @@
+package com.app.wildhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildhealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/wildhealth/WildhealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.wildhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildhealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
