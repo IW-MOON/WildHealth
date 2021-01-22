8bd0741221c2bbdb0635005daee097f0b9e6c29f
IW.MOON <=>
2021-01-22 20:25:21 +0900

    init

--- src/main/java/com/app/wildhealth/WildhealthApplication.java
+++ src/main/java/com/app/wildhealth/WildhealthApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WildhealthApplication {
 
-	//dfsf
+	//dfsfdfdsf
 	public static void main(String[] args) {
 		SpringApplication.run(WildhealthApplication.class, args);
 	}
